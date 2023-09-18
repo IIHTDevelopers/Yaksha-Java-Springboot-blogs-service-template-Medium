2f5035366a1847e985bd2b21028f95e5476cbca2
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-09-18 11:40:11 +0530

    Delete src directory

--- src/main/java/com/iiht/training/blogs/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package com.iiht.training.blogs.service;
-
-import com.iiht.training.blogs.dto.CommentDto;
-
-public interface CommentService {
-
-	public CommentDto postComment(CommentDto commentDto);
-}
