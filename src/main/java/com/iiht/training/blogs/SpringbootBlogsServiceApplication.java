7da794a055004c741c611c2e78d971bd94c4bc7e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-09-18 11:44:34 +0530

    Add files via upload

---
+++ src/main/java/com/iiht/training/blogs/SpringbootBlogsServiceApplication.java
@@ -0,0 +1,14 @@
+package com.iiht.training.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogsServiceApplication.class, args);
+	}
+
+	// Please add model mapper bean here if you want only then
+}
